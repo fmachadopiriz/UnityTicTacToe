bf12d166f57cb9f5612b52dc390fd8228515e70e
fmachadopiriz <fmachadopiriz@outlook.com>
2018-09-08 18:14:54 -0300

    Penúltimo paso completado

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,6 +9,21 @@ public class GameController : MonoBehaviour
 
     private string playerSide;
 
+    public GameObject gameOverPanel;
+
+    public Text gameOverText;
+    private int moveCount;
+
+    public GameObject restarButton;
+
+    public Player playerX;
+
+    public Player playerO;
+
+    public PlayerColor activePlayerColor;
+
+    public PlayerColor inactivePlayerColor;
+
     void SetGameControllerReferenceOnButtons()
     {
         for (int i = 0; i < buttonList.Length; i++)
@@ -21,6 +36,10 @@ public class GameController : MonoBehaviour
     {
         this.SetGameControllerReferenceOnButtons();
         this.playerSide = "X";
+        this.gameOverPanel.SetActive(false);
+        this.moveCount = 0;
+        this.restarButton.SetActive(false);
+        this.SetPlayerColors(playerX, playerO);
     }
 
     public string GetPlayerSide()
@@ -35,25 +54,83 @@ public class GameController : MonoBehaviour
 
     public void EndTurn()
     {
+        this.moveCount++;
+
         if (CheckButtons(0, 1, 2) || CheckButtons(3, 4, 5) || CheckButtons(6, 7, 8) ||
             CheckButtons(0, 3, 6) || CheckButtons(1, 4, 7) || CheckButtons(2, 5, 8) ||
             CheckButtons(0, 4, 8) || CheckButtons(2, 4, 6))
         {
-            this.GameOver();
+            this.GameOver(playerSide);
         }
-        ChangeSides();
+        else if (this.moveCount >= 9)
+        {
+            this.GameOver("draw");
+        }
+        else
+        {
+            ChangeSides();
+        }
     }
 
-    void GameOver()
+    void GameOver(string winningPlayer)
     {
-        for (int i = 0; i < buttonList.Length; i++)
+        this.SetBoardInteractable(false);
+        if (winningPlayer == "draw")
         {
-            buttonList[i].GetComponentInParent<Button>().interactable = false;
+            this.SetGameOverText("It's a draw");
         }
+        else
+        {
+            this.SetGameOverText(playerSide + " Wins!");
+        }
+        this.restarButton.SetActive(true);
     }
 
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "O" : "X";
+        if (playerSide == "X")
+        {
+            this.SetPlayerColors(this.playerX, this.playerO);
+        }
+        else
+        {
+            this.SetPlayerColors(this.playerO, this.playerX);
+        }
+    }
+
+    void SetGameOverText(string value)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = value;
+    }
+
+    void SetBoardInteractable(bool toggle)
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+        }
+    }
+
+    public void RestartGame()
+    {
+        this.playerSide = "X";
+        this.moveCount = 0;
+        this.gameOverPanel.SetActive(false);
+        this.SetBoardInteractable(true);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].text = "";
+        }
+        this.restarButton.SetActive(false);
+    }
+
+    public void SetPlayerColors(Player newPlayer, Player oldPlayer)
+    {
+        newPlayer.panel.color = activePlayerColor.panelColor;
+        newPlayer.text.color = activePlayerColor.textColor;
+        oldPlayer.panel.color = inactivePlayerColor.panelColor;
+        oldPlayer.text.color = inactivePlayerColor.textColor;
     }
 }
---
+++ Assets/Scripts/PlayerColor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class PlayerColor
+{
+    public Color panelColor;
+    public Color textColor;
+}
